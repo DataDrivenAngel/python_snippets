0dc68b0548ad2fcefbd71f7304930c0cf72c28dc
angel <buildingwill@gmail.com>
2020-11-08 23:23:40 -0500

    add 1

---
+++ springer_exercises/1/1.py
@@ -0,0 +1,13 @@
+# Exercise 1:
+
+# Create a program that displays your name and complete mailing address formatted in
+# does not need to read any input from the user.
+# the manner that you would usually see it on the outside of an envelope. Your program
+
+name = "Yours Truly"
+address = "123 Main St"
+state = "MD"
+zip = "20814"
+
+print(name +"\n" + address +"\n"+ state +"\n"+ zip)
+
