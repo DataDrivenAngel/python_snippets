8154cf747bd650eab0d785997a42676589fdcf3d
angel <buildingwill@gmail.com>
2020-11-08 23:24:05 -0500

    add 2

---
+++ springer_exercises/1/2.py
@@ -0,0 +1,8 @@
+# Exercise 2: Hello
+# (9 Lines)
+# Write a program that asks the user to enter his or her name. The program should
+# respond with a message that says hello to the user, using his or her name.
+
+
+name = input("What is your name?")
+print ("Nice to meet you " + name)
