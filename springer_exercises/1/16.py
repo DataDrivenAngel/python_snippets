a752a9df81e2bc8cfb2cba6f14405fcfad97e7a9
angel <buildingwill@gmail.com>
2020-11-11 23:42:11 -0500

    add 16

---
+++ springer_exercises/1/16.py
@@ -0,0 +1,15 @@
+# Write a program that begins by reading a radius, r , from the user. The program will
+# continue by computing and displaying the area of a circle with radius r and the
+# volume of a sphere with radius r .
+
+
+pi = 3.14159265358979
+
+r = 1
+
+area = pi*(r ** 2)
+volume = (4/3)*pi*(r ** 3)
+
+
+print("Area = ", area, "\nVolume = ", volume)
+
