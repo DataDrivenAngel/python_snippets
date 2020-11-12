fd2deb2f4224963bcd63a54839560628ffbe90fe
angel <buildingwill@gmail.com>
2020-11-12 00:05:30 -0500

    add 21

---
+++ springer_exercises/1/21.py
@@ -0,0 +1,9 @@
+# Write a program that allows the user to enter values for b and h. The program
+# should then compute and display the area of a triangle with base length b and height h.
+
+b = 5
+h = 5
+
+area = (b*h)/2
+
+print(area)
