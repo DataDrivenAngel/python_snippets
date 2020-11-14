17ed92fe6ac4f706e83593f3e54afc7f0eada053
angel <buildingwill@gmail.com>
2020-11-14 10:45:49 -0500

    add 28

---
+++ springer_exercises/1/28.py
@@ -0,0 +1,10 @@
+# Write a program that begins by reading the air temperature and wind speed from the
+# user. Once these values have been read your program should display the wind chill
+# index rounded to the closest integer.
+
+temp = -10
+speed = 10
+
+wci = 13.12 + (0.6215 * temp) - (11.37 * (speed ** 0.16)) + (0.3965 * (temp ** 0.16))
+
+print(wci)
