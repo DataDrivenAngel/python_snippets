a7b74610cf038daf7d3505ea8b3c42344bacfa35
angel <buildingwill@gmail.com>
2020-11-14 10:39:47 -0500

    add 27

---
+++ springer_exercises/1/27.py
@@ -0,0 +1,16 @@
+# Write a program that computes the body mass index (BMI) of an individual. Your
+# program should begin by reading a height and weight from the user. Then it should
+# This copy belongs to use one of the following two formulas to compute the BMI before displaying it.
+
+
+height = 1.9
+weight = 60
+
+bmi = weight/(height ** 2)
+print(bmi)
+
+height  = 69
+weight = 150
+
+bmi = (weight/(height**2))*703
+print (bmi)
