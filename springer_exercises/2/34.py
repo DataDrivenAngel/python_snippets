30064fa432469066bfc8101f430431643d2031fb
angel <buildingwill@gmail.com>
2020-11-16 22:38:18 -0500

    add 34

---
+++ springer_exercises/2/34.py
@@ -0,0 +1,10 @@
+# Write a program that reads an integer from the user. Then your program should
+# display a message indicating whether the integer is even or odd.
+#
+
+user_input = 5
+
+if user_input % 2 == 0:
+    print("even")
+else:
+    print("odd")
