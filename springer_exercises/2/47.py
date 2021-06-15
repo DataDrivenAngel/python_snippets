0c1b2bcf23d298bb44481f1c5c0617be2ca0db2e
angel <buildingwill@gmail.com>
2021-06-14 21:52:58 -0400

    init 47

---
+++ springer_exercises/2/47.py
@@ -0,0 +1,30 @@
+# Write a program that asks the user to enter his or her month and day of birth. 
+# Then your program should report the user’s zodiac sign as part of an appropriate output message.
+
+# Zodiac sign 
+# Capricorn
+# Aquarius
+# Pisces
+# Aries
+# Taurus
+# Gemini
+# Cancer
+# Leo
+# Virgo
+# Libra
+# Scorpio
+# Sagittarius
+
+# 
+# January 20 to February 18
+# February 19 to March 20
+# March 21 to April 19
+# April 20 to May 20
+# May 21 to June 20
+# June 21 to July 22
+# July 23 to August 22
+# August 23 to September 22
+# September 23 to October 22
+# October 23 to November 21
+# November 22 to December 21
+
