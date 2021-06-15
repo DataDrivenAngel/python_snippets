aeac23b7fc633deb9116c2d4367c461fd3f60643
angel <buildingwill@gmail.com>
2021-06-14 22:05:37 -0400

    init 48

---
+++ springer_exercises/2/48.py
@@ -0,0 +1,4 @@
+# Write a program that reads a year from the user and displays the animal associated
+# with that year. Your program should work correctly for any year greater than or equal
+# to zero, not just the ones listed in the table
+
